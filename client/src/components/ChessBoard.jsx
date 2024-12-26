66579f6c7320b91794a1713213fa31a3b872dada
Ranit Jana <ranitjana100@gmail.com>
2024-12-26 21:02:38 +0530

    fix:3rd party move change

--- client/src/components/ChessBoard.jsx
+++ client/src/components/ChessBoard.jsx
@@ -27,8 +27,7 @@ export default function ChessBoard() {
   const { playerInfo } = useAuthContext();
 
   const isViewer = () => {
-    if (!playerInfo || players.player1._id) return true;
-    if (playerInfo._id != players.player1._id || playerInfo._id != players.player2._id) return true;
+    if (playerInfo._id != players.player1._id && playerInfo._id != players.player2._id) return true;
   }
 
   const navigate = useNavigate();
