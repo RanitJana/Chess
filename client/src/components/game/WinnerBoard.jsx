c7b4a43453d9de1a5890cfdece76c7f2e941b96e
Ranit Jana <ranitjana100@gmail.com>
2025-02-14 20:35:26 +0530

    feat:new animation type in complete games

--- client/src/components/game/WinnerBoard.jsx
+++ client/src/components/game/WinnerBoard.jsx
@@ -6,194 +6,19 @@ import GetAvatar from "../../utils/GetAvatar.js";
 import { useGameContext } from "../../pages/Game.jsx";
 import Toast from "../../utils/Toast.js";
 import { gameInit } from "../../api/game.js";
-
-function Canvas() {
-  const canvasRef = useRef(null);
-  const animationRef = useRef(null); // To track the animation frame
-
-  useEffect(() => {
-    const TWO_PI = Math.PI * 2;
-    const HALF_PI = Math.PI * 0.5;
-
-    let ctx;
-    let particles = [];
-    const timeStep = 1 / 60;
-    const viewWidth = 512;
-    const viewHeight = 512;
-
-    const Ease = {
-      outCubic: (t, b, c, d) => {
-        t /= d;
-        t--;
-        return c * (t * t * t + 1) + b;
-      },
-    };
-
-    const cubeBezier = (p0, p1, p2, p3, t) => {
-      const cX = 3 * (p1.x - p0.x);
-      const bX = 3 * (p2.x - p1.x) - cX;
-      const aX = p3.x - p0.x - cX - bX;
-
-      const cY = 3 * (p1.y - p0.y);
-      const bY = 3 * (p2.y - p1.y) - cY;
-      const aY = p3.y - p0.y - cY - bY;
-
-      const x = aX * Math.pow(t, 3) + bX * Math.pow(t, 2) + cX * t + p0.x;
-      const y = aY * Math.pow(t, 3) + bY * Math.pow(t, 2) + cY * t + p0.y;
-
-      return { x, y };
-    };
-
-    const Point = function (x, y) {
-      this.x = x || 0;
-      this.y = y || 0;
-    };
-
-    const Particle = function (p0, p1, p2, p3) {
-      this.p0 = p0;
-      this.p1 = p1;
-      this.p2 = p2;
-      this.p3 = p3;
-
-      this.time = 0;
-      this.duration = 3 + Math.random() * 2;
-      this.color = `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
-
-      this.w = 8;
-      this.h = 6;
-
-      this.complete = false;
-    };
-
-    Particle.prototype = {
-      update: function () {
-        this.time = Math.min(this.duration, this.time + timeStep);
-        const f = Ease.outCubic(this.time, 0, 1, this.duration);
-        const p = cubeBezier(this.p0, this.p1, this.p2, this.p3, f);
-
-        const dx = p.x - this.x;
-        const dy = p.y - this.y;
-
-        this.r = Math.atan2(dy, dx) + HALF_PI;
-        this.sy = Math.sin(Math.PI * f * 10);
-        this.x = p.x;
-        this.y = p.y;
-
-        this.complete = this.time === this.duration;
-      },
-      draw: function () {
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.r);
-        ctx.scale(1, this.sy);
-
-        ctx.fillStyle = this.color;
-        ctx.fillRect(-this.w * 0.5, -this.h * 0.5, this.w, this.h);
-
-        ctx.restore();
-      },
-    };
-
-    const initCanvas = () => {
-      const canvas = canvasRef.current;
-      if (!canvas) return;
-
-      canvas.width = viewWidth;
-      canvas.height = viewHeight;
-      ctx = canvas.getContext("2d");
-
-      createParticles();
-      startAnimation();
-    };
-
-    const createParticles = () => {
-      particles = [];
-      for (let i = 0; i < 128; i++) {
-        const p0 = new Point(viewWidth * 0.5, viewHeight * 0.5);
-        const p1 = new Point(
-          Math.random() * viewWidth,
-          Math.random() * viewHeight
-        );
-        const p2 = new Point(
-          Math.random() * viewWidth,
-          Math.random() * viewHeight
-        );
-        const p3 = new Point(Math.random() * viewWidth, viewHeight + 64);
-
-        particles.push(new Particle(p0, p1, p2, p3));
-      }
-    };
-
-    const loop = () => {
-      update();
-      draw();
-
-      if (particles.every((p) => p.complete)) {
-        createParticles();
-      }
-
-      animationRef.current = requestAnimationFrame(loop);
-    };
-
-    const startAnimation = () => {
-      cancelAnimationFrame(animationRef.current); // Stop any existing animation loop
-      animationRef.current = requestAnimationFrame(loop); // Start a new one
-    };
-
-    const stopAnimation = () => {
-      cancelAnimationFrame(animationRef.current);
-    };
-
-    const update = () => {
-      particles.forEach((p) => p.update());
-    };
-
-    const draw = () => {
-      ctx.clearRect(0, 0, viewWidth, viewHeight);
-      particles.forEach((p) => p.draw());
-    };
-
-    initCanvas();
-
-    const interval = setInterval(initCanvas, 5000); // Reinitialize every 2 seconds
-
-    return () => {
-      clearInterval(interval); // Clear interval on unmount
-      stopAnimation(); // Stop animation on unmount
-      particles = [];
-      ctx = null; // Clean up
-    };
-  }, []);
-
-  return (
-    <canvas
-      id="drawing_canvas"
-      ref={canvasRef}
-      style={{
-        position: "absolute",
-        width: "100%",
-        height: "100%",
-        top: "-50%",
-        left: "50%",
-        transform: "translateX(-50%)",
-        right: 0,
-        pointerEvents: "none",
-        zIndex: 9999,
-        // backgroundColor:"pink"
-      }}
-    ></canvas>
-  );
-}
+import Canvas from "../Canvas.jsx";
 
 function isYourWin(playerColor, isCheckMate) {
   return playerColor == isCheckMate || isCheckMate == winner.draw;
 }
 
-function closeContainer(reference, delay) {
-  if (!reference) return;
+function closeContainer(reference, parentContainerRef, delay) {
+  if (!reference || !parentContainerRef) return;
   reference.style.opacity = "0";
+  parentContainerRef.style.opacity = "0";
   setTimeout(() => {
-    reference.style.scale = "0";
+    reference.style.transform = "scale(0)";
+    parentContainerRef.style.transform = "scale(0)";
   }, delay);
 }
 
@@ -201,6 +26,7 @@ function WinnerBoard({ winnerReason, score }) {
   const { playerColor, isCheckMate, players } = useGameContext();
 
   const containerRef = useRef(null);
+  const parentContainerRef = useRef(null);
   const [isCreatingGame, setIsCreatingGame] = useState(false);
 
   const handleNewGame = async () => {
@@ -222,19 +48,38 @@ function WinnerBoard({ winnerReason, score }) {
     }
   };
 
+  useEffect(() => {
+    const containerReference = containerRef.current;
+    if (containerReference) {
+      containerReference.style.transform = "scale(0)";
+      parentContainerRef.current.style.opacity = "1";
+      setTimeout(() => {
+        containerReference.style.opacity = "1";
+        containerReference.style.transform = "scale(1)";
+      }, 150);
+    }
+  }, [winnerReason]);
+
   if (!isCheckMate) return;
 
   return (
     <div
-      ref={containerRef}
-      className="flex transition-all justify-center items-center w-dvw h-dvh fixed top-0 left-0 z-[9999] bg-[rgba(0,0,0,0.5)]"
+      ref={parentContainerRef}
+      className="flex duration-500 transition-opacity ease-out transform  justify-center items-center w-dvw h-dvh fixed top-0 left-0 z-[9999] bg-[rgba(0,0,0,0.5)]"
     >
-      <div className="relative bg-blackDarkest rounded-md text-white w-[min(100dvw,20rem)] flex flex-col items-center justify-center shadow-lg">
+      <div
+        ref={containerRef}
+        className="relative bg-blackDarkest transition-all scale-0 opacity-0 rounded-md text-white w-[min(100dvw,20rem)] flex flex-col items-center justify-center shadow-lg"
+      >
         <div className="flex justify-center items-center bg-blackLight p-4 w-[min(100dvw,20rem)] rounded-tl-md rounded-tr-md">
           <img
             src="/images/cross.png"
             onClick={(e) => {
-              closeContainer(containerRef.current, 150);
+              closeContainer(
+                containerRef.current,
+                parentContainerRef.current,
+                150
+              );
             }}
             alt=""
             className="z-50 absolute hover:cursor-pointer right-0 top-0 rounded-md w-8 p-1"
@@ -246,7 +91,7 @@ function WinnerBoard({ winnerReason, score }) {
             </div>
           ) : isYourWin(playerColor, isCheckMate) ? (
             <>
-              {<Canvas />}
+              <Canvas />
               <img src="/images/trophy.png" alt="" className="mr-2 w-8" />
               <div className="flex flex-col items-center justify-center">
                 <p className="font-bold text-xl uppercase">
